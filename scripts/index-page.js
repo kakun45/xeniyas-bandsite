ac6726e7cfe2d08fce5a0c31ef8ff20c3d159607
kakun45 <email4blackkat@gmail.com>
2023-02-13 17:21:11 -0500

    shows page, scss, js

---
+++ scripts/index-page.js
@@ -0,0 +1,23 @@
+// You must have an array in JavaScript with 3 default comment objects to start. Comments must have a name, a timestamp, and the comment text.
+let comment = [
+  {
+    name: "Connor Walton",
+    timestamp: "02/17/2021",
+    text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+  },
+  {
+    name: "Emilie Beach",
+    timestamp: "01/09/2021",
+    text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+  {
+    name: "Miles Acosta",
+    timestamp: "12/20/2020", // todo: new Date(),
+    text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  },
+];
+
+// window.onload();
+window.addEventListener("load", () => {
+  console.log("js of INDEX is loaded");
+});
