513cf634df8afbbea9b20e89998e3187afd85dd0
kakun45 <email4blackkat@gmail.com>
2023-02-13 22:24:51 -0500

    form outline

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,3 @@
+window.addEventListener("load", () => {
+  console.log("JS of SHOWS pge is loaded");
+});
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -3,17 +3,20 @@ let comment = [
   {
     name: "Connor Walton",
     timestamp: "02/17/2021",
-    text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+    comment:
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
   },
   {
     name: "Emilie Beach",
     timestamp: "01/09/2021",
-    text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+    comment:
+      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
   },
   {
     name: "Miles Acosta",
     timestamp: "12/20/2020", // todo: new Date(),
-    text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+    comment:
+      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
   },
 ];
 
