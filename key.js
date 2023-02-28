cfa763f2c446136f83d48400dccfb8f4ce87e467
kakun45 <email4blackkat@gmail.com>
2023-02-28 16:17:47 -0500

    key makes public

---
+++ key.js
@@ -0,0 +1,28 @@
+// this ia not secure, don't do it on real projects!!!
+const key = {
+  api_key: "b4fa038f-b57e-44af-9307-6494b1ee48f5",
+};
+
+// local db avatars if API is down, to use: uncomment strings
+// local test database of img for not hammering a picture server on reload
+let imgDB = [
+  // "https://images.dog.ceo/breeds/greyhound-italian/n02091032_12113.jpg",
+  // "https://images.dog.ceo/breeds/terrier-yorkshire/n02094433_2115.jpg",
+  // "https://images.dog.ceo/breeds/eskimo/n02109961_16311.jpg",
+  // "https://images.dog.ceo/breeds/bulldog-english/murphy.jpg",
+  // "https://images.dog.ceo/breeds/spaniel-sussex/n02102480_2041.jpg",
+  // "https://images.dog.ceo/breeds/ovcharka-caucasian/IMG_20190628_144817.jpg",
+  // "https://images.dog.ceo/breeds/dalmatian/cooper1.jpg",
+  // "https://images.dog.ceo/breeds/bulldog-french/n02108915_3258.jpg",
+  // "https://images.dog.ceo/breeds/malinois/n02105162_3925.jpg",
+  // "https://images.dog.ceo/breeds/sharpei/noel.jpg",
+  // "https://images.dog.ceo/breeds/hound-walker/n02089867_1599.jpg",
+  // "https://images.dog.ceo/breeds/maltese/n02085936_19133.jpg",
+  // "https://images.dog.ceo/breeds/terrier-cairn/n02096177_424.jpg",
+  // "https://images.dog.ceo/breeds/setter-gordon/n02101006_3125.jpg",
+  // "https://images.dog.ceo/breeds/springer-english/n02102040_682.jpg",
+  // "https://images.dog.ceo/breeds/terrier-wheaten/n02098105_2004.jpg",
+  // "https://images.dog.ceo/breeds/whippet/n02091134_13356.jpg",
+  // "https://images.dog.ceo/breeds/dane-great/n02109047_5618.jpg",
+  // "https://images.dog.ceo/breeds/coonhound/n02089078_4544.jpg",
+];
